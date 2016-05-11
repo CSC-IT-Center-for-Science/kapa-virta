359b2896028031c711c5b766b3e8b6efe4d9a55d
Joni Ollikainen <joni.ollikainen@gofore.com>
2016-05-11 17:34:20 +0300

    Changed soap message handling from java classes to string. Implemented soap message manipulations from xroad to virta -direction.

--- src/main/java/kapaVirtaAS/VirtaClient/VirtaClient.java
+++ src/main/java/kapaVirtaAS/VirtaClient/VirtaClient.java
@@ -3,40 +3,134 @@ package kapaVirtaAS.VirtaClient;
 /**
  * Created by joni on 9.5.2016.
  */
+import org.apache.commons.lang3.StringUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ByteArrayEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
-import opiskelijatiedot.wsdl.*;
+import org.w3c.dom.*;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
-public class VirtaClient extends WebServiceGatewaySupport {
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.StringWriter;
+
+public class VirtaClient {
 
     private static final Logger log = LoggerFactory.getLogger(VirtaClient.class);
+    private static final String testSoapToVirta = "<x:Envelope xmlns:x=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:luk=\"http://tietovaranto.csc.fi/luku\"><x:Header/><x:Body><luk:LukukausiIlmoittautumisetRequest><luk:Kutsuja><luk:jarjestelma/><luk:tunnus/><luk:avain>salaisuus</luk:avain></luk:Kutsuja><luk:Hakuehdot><luk:kansallinenOppijanumero>d09afd87a8c6d76b76bbd</luk:kansallinenOppijanumero><luk:organisaatio>00001</luk:organisaatio></luk:Hakuehdot></luk:LukukausiIlmoittautumisetRequest></x:Body></x:Envelope>";
+    private static final String serviceName = "http://test.x-road.virta.csc.fi/producer";
+    private static final String virtaServiceName = "http://tietovaranto.csc.fi/luku";
 
-    public Object getVirtaWS(String xroadRequest){
-        return getWebServiceTemplate().marshalSendAndReceive(xroadRequest);
+    public static HttpResponse getVirtaWS(String XRoadRequest) {
+        String manipulatedXRoadRequest = virtaRequestManipulator(XRoadRequest);
+
+        HttpClient client = HttpClientBuilder.create().build();
+
+        //HTTP post
+        HttpPost post = new HttpPost("http://virtawstesti.csc.fi:80/luku106/OpiskelijanTiedot");
+        post.setHeader("Content-type","text/xml");
+        //SOAP message to HTTP body
+        HttpEntity entity = new ByteArrayEntity(manipulatedXRoadRequest.getBytes());
+        post.setEntity(entity);
+
+        try {
+            return client.execute(post);
+        }
+        catch(IOException e) {
+            log.error(e.toString());
+        }
+        return null;
     }
 
-    public TutkinnotResponse getVirtaResponse(TutkinnotRequest request){
-        return (TutkinnotResponse) getWebServiceTemplate().marshalSendAndReceive(request);
-    }
+    // Removes XRoad headers from XRoadRequest
+    private static String virtaRequestManipulator(String XRoadRequest){
+        try {
+            DocumentBuilder dBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            Document doc = dBuilder.parse(new InputSource(new ByteArrayInputStream(XRoadRequest.getBytes())));
 
-    public OpiskeluoikeudetResponse getVirtaResponse(OpiskeluoikeudetRequest request){
-        return (OpiskeluoikeudetResponse) getWebServiceTemplate().marshalSendAndReceive(request);
-    }
+            Element root = doc.getDocumentElement();
 
-    public OpiskelijanTiedotResponse getVirtaResponse(OpiskelijanTiedotRequest request){
-        return (OpiskelijanTiedotResponse) getWebServiceTemplate().marshalSendAndReceive(request);
-    }
+            root.removeAttribute("xmlns:xro");
+            String att = root.getAttribute(serviceName);
 
-    public OpiskelijanKaikkiTiedotResponse getVirtaResponse(OpiskelijanKaikkiTiedotRequest request){
-        return (OpiskelijanKaikkiTiedotResponse) getWebServiceTemplate().marshalSendAndReceive(request);
-    }
 
-    public OpintosuorituksetResponse getVirtaResponse(OpintosuorituksetRequest request){
-        return (OpintosuorituksetResponse) getWebServiceTemplate().marshalSendAndReceive(request);
-    }
+            //Change tns schema
+            NamedNodeMap attributes = root.getAttributes();
+            for(int i = 0; i < attributes.getLength(); ++i) {
+                Node attribute = attributes.item(i);
+                String value = attribute.getNodeValue();
+                if(attribute.getNodeValue().contains(serviceName)){
+                    attribute.setNodeValue(virtaServiceName);
+                }
+            }
 
-    public LukukausiIlmoittautumisetResponse getVirtaResponse(LukukausiIlmoittautumisetRequest request){
-        return (LukukausiIlmoittautumisetResponse) getWebServiceTemplate().marshalSendAndReceive(request);
+
+            NodeList children = root.getChildNodes();
+            for(int i = 0; i < children.getLength(); ++i){
+                //There should be two children; header and body
+                Node child = children.item(i);
+
+                //Header should have multiple children, body should have only one
+                if(child != null && child.getNodeName().toLowerCase().contains("header")){
+                    //Replace XRoad headers with empty SOAP header element with same name
+                    root.replaceChild(doc.createElement(child.getNodeName()),child);
+                }
+
+                //Add Request appendix to operation input name
+                //eg. Opintosuoritukset -> OpintosuorituksetRequest
+                if(child != null && child.getNodeName().toLowerCase().contains("body")){
+                    Node soapOperationNode = child.getFirstChild().getNextSibling();
+                    if(soapOperationNode.getNodeType() == Node.ELEMENT_NODE) {
+                        Element elem = (Element) soapOperationNode;
+                        String tagName = elem.getTagName();
+                        String tagNameForVirta = tagName+"Request";
+                        String nsUri = elem.getNamespaceURI();
+                        doc.renameNode(soapOperationNode, virtaServiceName, tagNameForVirta);
+                        elem.toString();
+                    }
+                }
+            }
+
+
+            DOMSource domSource = new DOMSource(doc);
+            StringWriter writer = new StringWriter();
+            StreamResult result = new StreamResult(writer);
+            Transformer transformer = TransformerFactory.newInstance().newTransformer();
+            transformer.transform(domSource, result);
+            XRoadRequest = writer.toString();
+        }
+        catch (IOException e){
+            log.error(e.toString());
+        }
+        catch (SAXException e){
+            log.error(e.toString());
+        }
+        catch (ParserConfigurationException e){
+            log.error(e.toString());
+        }
+        catch(TransformerException e){
+            log.error(e.toString());
+        }
+
+        //Remove xml definition element, if any
+        String sub = StringUtils.substringAfter(XRoadRequest, "?>");
+        if(sub != null && sub != ""){
+            return sub;
+        }
+        return XRoadRequest;
     }
 }
--- src/main/java/kapaVirtaAS/VirtaProvider/VirtaXRoadEndpoint.java
+++ src/main/java/kapaVirtaAS/VirtaProvider/VirtaXRoadEndpoint.java
@@ -1,116 +1,53 @@
 package kapaVirtaAS.VirtaProvider;
 
 import kapaVirtaAS.VirtaClient.VirtaClient;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.http.Header;
+import org.apache.http.HttpResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
 /**
  * Created by joni on 9.5.2016.
  */
 @RestController
 public class VirtaXRoadEndpoint {
-    @Autowired
-    private VirtaClient virtaclient;
+    private static final Logger log = LoggerFactory.getLogger(VirtaClient.class);
+    private final String[] headersToCopy = {"Content-Type", "Connection", "SOAPAction"};
 
     @RequestMapping(value="/ws", method= RequestMethod.POST)
-    public Object getVirtaResponse(@RequestBody String xroadReq){
-        return virtaclient.getVirtaWS(xroadReq);
+    public String getVirtaResponse(@RequestBody String xroadReq, HttpServletResponse response){
+        HttpResponse virtaResponse = VirtaClient.getVirtaWS(xroadReq);
+        for(Header virtaHeader : virtaResponse.getAllHeaders()){
+            if(Arrays.asList(headersToCopy).contains(virtaHeader.getName())){
+                response.setHeader(virtaHeader.getName(), virtaHeader.getValue());
+            }
+        }
+
+        try {
+            BufferedReader rd = new BufferedReader(
+                    new InputStreamReader(virtaResponse.getEntity().getContent()));
+
+            StringBuffer result = new StringBuffer();
+            String line;
+            while ((line = rd.readLine()) != null) {
+                result.append(line);
+            }
+            return result.toString();
+        }
+        catch(IOException e){
+            log.error(e.toString());
+            return null;
+        }
+
     }
-/*
-
-    private opiskelijatiedot.wsdl.HakuEhdotOrganisaatioVapaa mapHakuehdotOrganisaatioVapaa(HakuEhdotOrganisaatioVapaa hakuehdot) {
-        opiskelijatiedot.wsdl.HakuEhdotOrganisaatioVapaa mappedHakuehdot = new opiskelijatiedot.wsdl.HakuEhdotOrganisaatioVapaa();
-        mappedHakuehdot.setKansallinenOppijanumero(hakuehdot.getKansallinenOppijanumero());
-        mappedHakuehdot.setHenkilotunnus(hakuehdot.getHenkilotunnus());
-        return mappedHakuehdot;
-    }
-
-    private opiskelijatiedot.wsdl.OpiskelijanTiedotRequest.Hakuehdot mapOpiskelijanTiedotHakuehdot(OpiskelijanTiedot_Type.Hakuehdot hakuehdot) {
-        opiskelijatiedot.wsdl.OpiskelijanTiedotRequest.Hakuehdot mappedHakuehdot = new opiskelijatiedot.wsdl.OpiskelijanTiedotRequest.Hakuehdot();
-        mappedHakuehdot.setKansallinenOppijanumero(hakuehdot.getKansallinenOppijanumero());
-        mappedHakuehdot.setHenkilotunnus(hakuehdot.getHenkilotunnus());
-        return mappedHakuehdot;
-    }
-
-    private opiskelijatiedot.wsdl.Kutsuja mapKutsuja(Kutsuja kutsuja){
-        opiskelijatiedot.wsdl.Kutsuja mappedKutsuja = new opiskelijatiedot.wsdl.Kutsuja();
-        mappedKutsuja.setJarjestelma(kutsuja.getJarjestelma());
-        mappedKutsuja.setAvain(kutsuja.getAvain());
-        mappedKutsuja.setTunnus(kutsuja.getTunnus());
-        return mappedKutsuja;
-    }
-
-
-    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "Tutkinnot")
-    @ResponsePayload
-    public opiskelijatiedot.wsdl.TutkinnotResponse getTutkinnotResponse(@RequestPayload Tutkinnot xroadRequest){
-
-        opiskelijatiedot.wsdl.TutkinnotRequest virtaReq = new opiskelijatiedot.wsdl.TutkinnotRequest();
-
-        virtaReq.setHakuehdot(mapHakuehdotOrganisaatioVapaa(xroadRequest.getHakuehdot()));
-        virtaReq.setKutsuja(mapKutsuja(xroadRequest.getKutsuja()));
-
-        return virtaclient.getTutkinnot(virtaReq);
-    }
-
-    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "Opiskeluoikeudet")
-    @ResponsePayload
-    public opiskelijatiedot.wsdl.OpiskeluoikeudetResponse getOpiskeluoikeudetResponse(@RequestPayload Opiskeluoikeudet xroadRequest){
-        opiskelijatiedot.wsdl.OpiskeluoikeudetRequest virtaReq = new opiskelijatiedot.wsdl.OpiskeluoikeudetRequest();
-
-        virtaReq.setHakuehdot(mapHakuehdotOrganisaatioVapaa(xroadRequest.getHakuehdot()));
-        virtaReq.setKutsuja(mapKutsuja(xroadRequest.getKutsuja()));
-
-        return virtaclient.getVirtaWS((Object)virtaReq);
-    }
-
-    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "OpiskelijanTiedot")
-    @ResponsePayload
-    public Object getOpiskelijanTiedot(@RequestPayload OpiskelijanTiedot_Type xroadRequest){
-        opiskelijatiedot.wsdl.OpiskelijanTiedotRequest virtaReq = new opiskelijatiedot.wsdl.OpiskelijanTiedotRequest();
-
-        virtaReq.setHakuehdot(mapOpiskelijanTiedotHakuehdot(xroadRequest.getHakuehdot()));
-        virtaReq.setKutsuja(mapKutsuja(xroadRequest.getKutsuja()));
-
-        return virtaclient.getVirtaWS((Object)virtaReq);
-    }
-
-    //@PayloadRoot(namespace = NAMESPACE_URI, localPart = "OpiskelijanKaikkiTiedot")
-    @PayloadRoot(namespace = NAMESPACE_URI);
-    @ResponsePayload
-    public opiskelijatiedot.wsdl.OpiskelijanKaikkiTiedotResponse getOpiskelijanKaikkiTiedot(@RequestPayload OpiskelijanKaikkiTiedot xroadRequest){
-        opiskelijatiedot.wsdl.OpiskelijanKaikkiTiedotRequest virtaReq = new opiskelijatiedot.wsdl.OpiskelijanKaikkiTiedotRequest();
-
-        virtaReq.setHakuehdot(mapHakuehdotOrganisaatioVapaa(xroadRequest.getHakuehdot()));
-        virtaReq.setKutsuja(mapKutsuja(xroadRequest.getKutsuja()));
-
-        return virtaclient.getVirtaWS((Object)virtaReq);
-    }
-
-    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "Opintosuoritukset")
-    @ResponsePayload
-    public opiskelijatiedot.wsdl.OpintosuorituksetResponse getOpiskelijanKaikkiTiedot(@RequestPayload Opintosuoritukset xroadRequest){
-        opiskelijatiedot.wsdl.OpintosuorituksetRequest virtaReq = new opiskelijatiedot.wsdl.OpintosuorituksetRequest();
-
-        virtaReq.setHakuehdot(mapHakuehdotOrganisaatioVapaa(xroadRequest.getHakuehdot()));
-        virtaReq.setKutsuja(mapKutsuja(xroadRequest.getKutsuja()));
-
-        return virtaclient.getOpintosuoritukset(virtaReq);
-    }
-
-    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "LukukausiIlmoittautumiset")
-    @ResponsePayload
-    public opiskelijatiedot.wsdl.LukukausiIlmoittautumisetResponse getLukukausiIlmoittautumiset(@RequestPayload LukukausiIlmoittautumiset xroadRequest){
-        opiskelijatiedot.wsdl.LukukausiIlmoittautumisetRequest virtaReq = new opiskelijatiedot.wsdl.LukukausiIlmoittautumisetRequest();
-
-        virtaReq.setHakuehdot(mapHakuehdotOrganisaatioVapaa(xroadRequest.getHakuehdot()));
-        virtaReq.setKutsuja(mapKutsuja(xroadRequest.getKutsuja()));
-
-        return virtaclient.getLukukausiIlmoittautumiset(virtaReq);
-    }
-    */
 }
